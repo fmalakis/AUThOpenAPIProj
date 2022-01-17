da68ec5aacfde42d1ac1c89c73849e28b552d25f
fmalakis <xan.klem.30@gmail.com>
2022-01-17 19:42:04 +0200

    Rework department selection on main page
    This commit reworks the way the user selects their department of choice,
    as well as including a choice for the study progs of each individual
    department. This is done with the use of a Mat Stepper, to guide the
    user through their search.
    
    Mat Stepper is WIP.

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatStepper } from '@angular/material/stepper';
 import { Router } from '@angular/router';
-import { Department, ClassesService } from 'src/app/services/classes-service/classes.service';
+import { Department, ClassesService, StudiesProg } from 'src/app/services/classes-service/classes.service';
 import { UserServiceService } from 'src/app/services/user-service/user-service.service';
 
 @Component({
@@ -10,14 +12,21 @@ import { UserServiceService } from 'src/app/services/user-service/user-service.s
 })
 export class MainComponent {
 
+  @ViewChild('stepper') stepper: MatStepper;
+
   arrayOfDepartments: Department[];
+  arrayOfStudiesProg: StudiesProg[];
   userService: UserServiceService;
   classSservice: ClassesService;
   route: Router;
 
   searchString: string = "";
+  selectedDepartment: Department;
+  selectedStudyProg: StudiesProg;
+  depFormGroup: FormGroup;
+  studiesProgGroup: FormGroup;
 
-  constructor(userService: UserServiceService, classesService: ClassesService, route: Router) {
+  constructor(userService: UserServiceService, classesService: ClassesService, route: Router, private _formBuilder: FormBuilder) {
     this.userService = userService;
     this.classSservice = classesService;
     this.arrayOfDepartments = [];
@@ -28,15 +37,38 @@ export class MainComponent {
     this.classSservice.getAllFaculties().then((departments) => {
       this.arrayOfDepartments = departments;
     });
+
+    this.depFormGroup = this._formBuilder.group({
+      depCtrl: ['', Validators.required]
+    });
+
+    this.studiesProgGroup = this._formBuilder.group({
+      studyCtrl: ['', Validators.required]
+    });
+
   }
 
-  navigateToDepartment(dep: Department) {
-    this.route.navigate(['/department', dep.id]).then(() => {
-      this.classSservice.setCurrentDepartment(dep);
+  navigateToPresentation() {
+    this.route.navigate(['/department', this.selectedDepartment.id, this.selectedStudyProg.prID]).then(() => {
+      this.classSservice.setCurrentDepartment(this.selectedDepartment);
     });
   }
 
+  selectDepartment(dep: Department) {
+    this.selectedDepartment = dep;
+    this.stepper.next();
+  }
 
+  navigateToStudies() {
+    this.classSservice.getDeptStudiesProg(this.selectedDepartment.id).then((studiesProgs) => {
+      this.arrayOfStudiesProg = studiesProgs;
+      this.stepper.next();
+    })
+  }
+
+  check() {
+    console.log(this.selectedDepartment);
+  }
   
 
 }
